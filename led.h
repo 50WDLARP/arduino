a28b980d71f57bebf1b58ee94485a05a2555429c
aberman <arthurberman94@gmail.com>
2016-03-30 22:22:15 -0400

    Add gamestate

--- led.h
+++ led.h
@@ -18,16 +18,19 @@ class Led {
 public:
     void led_setup();
     void led_loop(int mode);
+    void setColorRGB(int r, int g, int b);
     int led_intensity;
-
+    int current_scroll;
 private:
     CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
+    CRGB current_color;
     CRGB *led_leds = leds_plus_safety_pixel + 1;
     uint16_t XY( uint8_t x, uint8_t y);
     uint16_t XYsafe( uint8_t x, uint8_t y);
     void DrawOneFrame( byte startHue8, int8_t yHueDelta8, int8_t xHueDelta8);
     void DrawNoise();
-    void DrawScroll(int current);
+    void DrawScroll();
     void DrawIntensity();
+    void DrawColor();
 };
 #endif
