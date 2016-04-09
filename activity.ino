7d49fdea1166476a687a238ec7b274074ddfef1d
Rachael Robinson <Rachael@resnet157-19.medford.tufts.edu>
2016-04-09 17:57:02 -0400

    commented to clarify code

--- activity.ino
+++ activity.ino
@@ -4,25 +4,28 @@
  */
 
 unsigned long time; //used as a timer to graph activity and time (micros(): max 70 minutes)
-float prev_acc = 0;
+float prev_acc = 0; //tracks the acceleration previously read
 void setup() {
     Serial.begin(57600);
 }
 
 void loop() {
     /*Activity Tracker*/
-    time = micros();
-    float seconds = time/1000000.0;
-    AccelerationReading reading = Bean.getAcceleration();
-    char x = abs(reading.xAxis) / 2;
+    time = micros(); //tracks time from the beginning of the program
+    float seconds = time/1000000.0; //converts to seconds - we can disregard and only track micros is more convenient for graphing. 
+    AccelerationReading reading = Bean.getAcceleration(); //takes acceleration from Bean
+    /* extracts the acceleration from each axis from the reading */
+    char x = abs(reading.xAxis) / 2; 
     char y = abs(reading.yAxis) / 2;
     char z = abs(reading.zAxis) / 2;
 //    Bean.setLed(0,255,0);
+    /* Finds the average acceleration */
     float acc = sqrt(x*x + y*y + z*z);
     float deltaAcc = abs(acc - prev_acc);
-    if(deltaAcc > 10.0){
+    if(deltaAcc > 10.0){ //seems to be the threshold for substantial movements
         Bean.setLed(0,0,255);
     }
+    /* logs information to Serial */ 
     Serial.print(seconds);
     Serial.print(":");
     Serial.println(deltaAcc);
